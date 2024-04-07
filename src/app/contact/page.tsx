cd40c373d2f17448904bbaae5c877d76aba65caf
Niall Sharma <Niall.k.sharma@gmail.com>
2024-04-07 18:36:31 -0500

    dfasf

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ export default function Projects() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24 scrollbar-hide">
         <div>
-          <h1 className="">Contact</h1>
+          <h1 className="text-white">Contact</h1>
         </div>
            
       </main>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,7 @@ export default function Projects() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div>
-          <h1 className="">Projects</h1>
+          <h1 className="text-white">Projects</h1>
         </div>
            
       </main>
