15611ab04fd68dfb9956e52060cbf6140b7631e0
Niall Sharma <Niall.k.sharma@gmail.com>
2024-04-07 11:39:59 -0500

    surew

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en" className="scrollbar-hide">
       <Navbar />
       {children}
+      <Footer />
     </html>
   );
 }
