a5108447f06765ae31a6c3fd8ee3d9ea385bf2ba
Niall Sharma <Niall.k.sharma@gmail.com>
2024-04-07 10:47:30 -0500

    fixed stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@
 export default function Home() {
   return (
     <main className=" ">
-      <div className="bg-fixed flex w-screen text-8xl font-bold justify-center items-center align-middle text-center bg-center bg-cover to-black h-screen from-slate-800 bg-gradient-radial">
-        <h1 className="">Niall Sharma</h1>
+      <div className="bg-fixed flex items-center align-middle justify-center text-8xl font-bold  bg-center bg-cover to-black h-screen from-slate-800 bg-gradient-radial">
+        <h1 className="text-center ">Niall Sharma</h1>
+
       </div>
-      <div className="p-20 bg-slate-950">
-        <p className="text-cyan-500">HELP</p>
+      <div className="p-20 bg-stone-900">
+        <p className="text-cyan-500">Hi</p>
       </div>   
     </main>
   );
