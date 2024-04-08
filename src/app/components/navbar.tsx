f2a0bbeaff8e488673965dea9e9977e7fb1a9c83
Niall Sharma <Niall.k.sharma@gmail.com>
2024-04-07 20:55:58 -0500

    cleaned up superfluous classes

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,7 +3,7 @@ import NavButton from "./navbutton";
 export default function Navbar() {
     return(
         <>
-            <nav className="absolute top-0 drop-shadow-xl flex flex-wrap  py-3 bg-transperant items-right align-right">
+            <nav className="absolute top-0 flex flex-wrap  py-3 bg-transperant items-right align-right">
                 <div>
                     <ul className="flex flex-col lg:flex-row">
                         <NavButton pageLink="/" pageName="Home" />
