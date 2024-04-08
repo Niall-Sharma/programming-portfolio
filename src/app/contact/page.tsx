262acc8b17148cb9785ba238a615674793c55aa1
Niall Sharma <Niall.k.sharma@gmail.com>
2024-04-07 22:01:08 -0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 export default function Projects() {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 scrollbar-hide">
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 scrollbar-hide bg-stone-900">
         <div>
           <h1 className="text-white">Contact</h1>
         </div>
