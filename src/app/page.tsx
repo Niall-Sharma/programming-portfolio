a935a69d647d2f54bf853679d47b2b90217d5306
Niall Sharma <Niall.k.sharma@gmail.com>
2024-04-07 20:40:25 -0500

    ddasf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Niall from "../../public/dummy_1080x720_000000_a3b7e2.png"
 export default function Home() {
   return (
     <main className="scrollbar-hide">
-      <div className="flex items-center align-middle justify-center text-8xl font-bold bg-center bg-cover to-black h-screen bg-gradient-radial from-purple-950 bg-fixed">
-        <h1 className="text-white uppercase"><span className="text-2xl">Hi! my name is </span><br /> Niall <br />Sharma </h1>
+      <div className="flex items-center align-middle justify-center font-bold bg-center bg-cover to-black min-h-screen bg-gradient-radial from-purple-950 bg-fixed">
+        <h1 className="text-white uppercase text-8xl"><span className="text-2xl">Hi! my name is </span><br /> Niall <br />Sharma </h1>
       </div>
-      <div className="p-20 bg-stone-900 flex flex-wrap space-x-20 items-center justify-center">
+      <div className="min-h-screen bg-stone-900 flex flex-wrap space-x-20 items-center justify-center">
           <div className="w-full md:w-1/3">
             <p className="text-white text-lg">Quis aliquip amet sit fugiat duis dolor. Eiusmod laboris est laborum aliquip velit eiusmod. Qui nostrud reprehenderit consectetur sunt velit deserunt nisi et esse nulla ipsum. Quis labore esse occaecat nulla mollit in proident. Do sunt mollit nulla Lorem irure incididunt non in. Do est exercitation ea cupidatat sunt ullamco. Minim ut do irure labore irure aute consectetur esse sit eiusmod ex sunt sunt amet.</p>
           </div>
