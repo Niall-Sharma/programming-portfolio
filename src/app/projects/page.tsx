fbaa0e422c99f7c22d8aa93db9bcb862e0bd0d29
Niall Sharma <Niall.k.sharma@gmail.com>
2024-04-07 23:40:06 -0500

    fixed responsive

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import dummyImage from "../../../public/dummy_1080x720_000000_a3b7e2.png"
 export default function Projects() {
     return (
       <main className=" min-h-screen items-center justify-between p-24 bg-stone-900">
-        <div className="grid-cols-4 gap-4 grid">
+        <div className="grid-cols-2 lg:grid-cols-3 gap-4 grid">
           <ProjectCard pageLink="/" imageLink={dummyImage} projectName="Test 1" />
           <ProjectCard pageLink="/" imageLink={dummyImage} projectName="Test 2" />
           <ProjectCard pageLink="/" imageLink={dummyImage} projectName="Test 3" />
