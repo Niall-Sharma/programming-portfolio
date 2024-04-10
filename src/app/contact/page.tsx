d36706c2f0b8fa1820aeb9c2c7e55512e655a8e1
Niall Sharma <Niall.k.sharma@gmail.com>
2024-04-09 21:48:04 -0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,70 @@
 export default function Contact() {
-    return (
-      <main className=" min-h-screen items-center justify-between p-24 bg-stone-900">
+  return (
+    <main className=" min-h-screen items-center p-24 bg-stone-900 flex justify-center">
+      <div className="bg-stone-800 p-20 drop-shadow-xl w-full">
+        <form>
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label htmlFor="first-name" className="block text-md font-bold leading-6 text-white">
+                First name
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="block w-full p-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-      </main>
-    );
-  }
-  
+            <div className="sm:col-span-3">
+              <label htmlFor="last-name" className="block text-md font-bold leading-6 text-white">
+                Last name
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="block p-1 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label htmlFor="email" className="block text-md font-bold leading-6 text-white">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full p-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="w-full col-span-full">
+              <label htmlFor="message" className="block text-md font-bold leading-8 text-white">
+                  Message
+              </label>
+              <textarea id="message" className="text-black p-2 block w-full" rows={4} />
+            </div>
+          </div>
+          <div className="mt-12 flex items-center justify-center gap-x-6">
+          <button
+            type="submit"
+            className="rounded-sm outline outline-1 hover:text-white hover:ease-in-out transition-all outline-purple-500 px-4 py-2 text-lg font-thin text-purple-500 shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-500"
+          >
+            Submit
+          </button>
+        </div>
+        </form>
+      </div>
+    </main>
+  );
+}
