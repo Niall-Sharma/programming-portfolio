d14d2809e3df733f1536688a4a7fc38e4606e570
Niall Sharma <Niall.k.sharma@gmail.com>
2024-04-14 20:04:30 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
     <main className="scrollbar-hide flex justify-center flex-wrap">
       <header className="flex w-full items-center flex-wrap align-middle justify-center font-bold bg-center bg-cover to-black min-h-screen bg-gradient-radial from-purple-950 bg-fixed">
         <div className="animate-fade-up animate-once animate-ease-out">
-          <h1 className="text-white uppercase text-6xl sm:text-8xl text-wrap "><span className="text-2xl">Hi! my name is </span><br /> Niall <br />Sharma </h1>
+          <h1><span className="text-3xl">Hi! My name is </span></h1>
+          <h1 className="text-white uppercase text-6xl sm:text-8xl text-wrap"> Niall <br />Sharma <br /></h1>
+          <h1 className="text-xl leading-loose">I'm A Full Stack Developer</h1>
           <svg className="invert w-10 h-10 mt-10 animate-bounce ml-auto mr-auto" width="128" height="128" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M24 12c0-6.623-5.377-12-12-12s-12 5.377-12 12 5.377 12 12 12 12-5.377 12-12zm-1 0c0-6.071-4.929-11-11-11s-11 4.929-11 11 4.929 11 11 11 11-4.929 11-11zm-11.5 4.828l-3.763-4.608-.737.679 5 6.101 5-6.112-.753-.666-3.747 4.604v-11.826h-1v11.828z"/></svg>
         </div>
       </header>
