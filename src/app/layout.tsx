924bbb361b26e12898953ff6b0697befaaa6721b
Niall Sharma <Niall.k.sharma@gmail.com>
2024-11-14 11:26:33 -0600

    got rid of hidden scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scrollbar-hide">
+    <html lang="en" className="">
       <body>
       <Navbar />
       {children}
