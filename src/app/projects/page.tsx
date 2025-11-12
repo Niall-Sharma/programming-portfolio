0d94e0b66bb78b90735c7472669bc2492470ad1b
Niall Sharma <niall.k.sharma@gmail.com>
2025-11-12 13:53:50 -0600

    added projects view back into site with rust-cli

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,17 +1,38 @@
 import ProjectCard from "../components/projectcard";
-import dummyImage from "../../../public/dummy_1080x720_000000_a3b7e2.png"
-import PlantGameImage from "../../../public/PlantGame.png"
-import SpaceShotImage from "../../../public/SpaceShotImage.png"
+import dummyImage from "../../../public/dummy_1080x720_000000_a3b7e2.png";
+import PlantGameImage from "../../../public/PlantGame.png";
+import SpaceShotImage from "../../../public/SpaceShotImage.png";
 export default function Projects() {
     return (
-      <main className="flex min-h-screen items-center justify-center md:p-24 bg-stone-900 flex-wrap">
-        <div className="grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 grid items-start justify-items-center align-middle">
-          <ProjectCard projecttype="Video Game" githubLink="https://github.com/Niall-Sharma/PlantGame/" pageLink="https://sirtago.itch.io/plant-frenzy" imageLink={PlantGameImage} projectName="Plant Frenzy" projectDescription="A cozy game about a plant enthusiant growing plants in their own home. Made in Godot using the C# language." />
-          <ProjectCard projecttype="Video Game" githubLink="https://github.com/Niall-Sharma/EndlessMobileGame" pageLink="https://sirtago.itch.io/spaceshot" imageLink={SpaceShotImage} projectName="Space Shot" projectDescription="An endless game where you are trying to hit as many planets as you can. Can you beat my high score of 38? Made in Godot using GDScript." />
-          {/* <ProjectCard projecttype="Research App" githubLink="https://github.com/Niall-Sharma/GMResearchGame/" pageLink="/" imageLink={dummyImage} projectName="Emotion Collection GeoGame" projectDescription="Done as part of a research project in the Game Design Major at Iowa State University. This project is a game that is meant to collect the emotional value of a place from users." /> */}
-          {/*<ProjectCard projecttype="Web App" githubLink="/" pageLink="/" imageLink={dummyImage} projectName="Test 4" projectDescription="Eiusmod ex esse dolore aute ut consectetur incididunt." /> */}
-        </div>
-      </main>
+        <main className="flex min-h-screen items-center justify-center md:p-24 bg-stone-900 flex-wrap">
+            <div className="grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 grid items-start justify-items-center align-middle">
+                <ProjectCard
+                    projecttype="Video Game"
+                    githubLink="https://github.com/Niall-Sharma/PlantGame/"
+                    pageLink="https://sirtago.itch.io/plant-frenzy"
+                    imageLink={PlantGameImage}
+                    projectName="Plant Frenzy"
+                    projectDescription="A cozy game about a plant enthusiant growing plants in their own home. Made in Godot using the C# language."
+                />
+                <ProjectCard
+                    projecttype="Video Game"
+                    githubLink="https://github.com/Niall-Sharma/EndlessMobileGame"
+                    pageLink="https://sirtago.itch.io/spaceshot"
+                    imageLink={SpaceShotImage}
+                    projectName="Space Shot"
+                    projectDescription="An endless game where you are trying to hit as many planets as you can. Can you beat my high score of 38? Made in Godot using GDScript."
+                />
+                <ProjectCard
+                    projecttype="App"
+                    githubLink="https://github.com/Niall-Sharma/RustCliTodo"
+                    pageLink=""
+                    imageLink={PlantGameImage}
+                    projectName="CLI To-Do List in Rust"
+                    projectDescription="A simple command-line to-do list application written in Rust. Manage your tasks directly from your terminal—add, list, complete, and remove them with ease!"
+                />
+                {/*<ProjectCard projecttype="Web App" githubLink="/" pageLink="/" imageLink={dummyImage} projectName="Test 4" projectDescription="Eiusmod ex esse dolore aute ut consectetur incididunt." /> */}
+            </div>
+        </main>
     );
-  }
-  
+}
+
